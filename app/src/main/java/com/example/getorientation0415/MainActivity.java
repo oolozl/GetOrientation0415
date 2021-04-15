56d4fe3dfb90f01cdb9ec69bd7be29819483c854
S-B choi <sb112233@naver.com>
2021-04-15 15:03:19 +0900

    savePower

--- app/src/main/java/com/example/getorientation0415/MainActivity.java
+++ app/src/main/java/com/example/getorientation0415/MainActivity.java
@@ -1,22 +1,27 @@
 package com.example.getorientation0415;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
     TextView txtAzimuth, txtPitch, txtRoll;
     SensorManager sensorManager;
     Sensor magSensor, accSensor;
     SensorEventListener listener;
 
-    float[] magValue, accValue;
+    float[] magValues, accValues;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,50 +30,77 @@ public class MainActivity extends AppCompatActivity {
 
         txtAzimuth = findViewById(R.id.txtAzimuth);
         txtPitch = findViewById(R.id.txtPitch);
-        txtPitch = findViewById(R.id.txtRoll);
+        txtRoll = findViewById(R.id.txtRoll);
 
-        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
         magSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
         accSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
         listener = new SensorEventListener() {
             @Override
             public void onSensorChanged(SensorEvent sensorEvent) {
+
                 switch (sensorEvent.sensor.getType()) {
+
                     case Sensor.TYPE_ACCELEROMETER:
-                        accValue = sensorEvent.values.clone(); break;
+                        accValues = sensorEvent.values.clone(); break;
+
                     case Sensor.TYPE_MAGNETIC_FIELD:
-                        magValue = sensorEvent.values.clone(); break;
+                        magValues = sensorEvent.values.clone(); break;
+
                 }
-                if (magValue != null && accValue != null) {
-                    // 1) 회전(Rotation) 행렬과 경사(Inclination) 행렬 얻기
+
+                if (magValues != null && accValues != null) {
+
+
                     float[] R = new float[16]; //얻고자 하는 회전 행렬 (장비의 방향을 계산할 때 이용)
                     float[] I = new float[16]; //얻고자 하는 경사 행렬 (장비의 경사 각도를 계산할 때 이용)
-                    SensorManager.getRotationMatrix(R, I, accValue, magValue);
 
-                    // 2) 회전행렬로부터 방향 얻기
+                    SensorManager.getRotationMatrix(R, I, accValues, magValues);
+
                     float[] values = new float[3];
+
                     SensorManager.getOrientation(R, values);
+
                     if((int) radian2Degree(values[0]) == 180) {
+
                         Toast.makeText(MainActivity.this, "180", Toast.LENGTH_SHORT);
+
                     } else if((int) radian2Degree(values[0]) == -180) {
+
                         Toast.makeText(MainActivity.this, "-180", Toast.LENGTH_SHORT);
+
                     }
+
                     txtAzimuth.setText("Azimuth: " + (int) radian2Degree(values[0]));
-
                     txtPitch.setText("Pitch: " + (int) radian2Degree(values[1]));
-
                     txtRoll.setText("Roll: " + (int) radian2Degree(values[2]));
+
                 }
             }
+
             @Override
-            public void onAccuracyChanged(Sensor sensor, int accuracy) {
-            }
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {}
         };
+
+        sensorManager.registerListener(listener, magSensor, SensorManager.SENSOR_DELAY_UI);
+        sensorManager.registerListener(listener, accSensor, SensorManager.SENSOR_DELAY_UI);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(listener);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
         sensorManager.registerListener(listener, magSensor, SensorManager.SENSOR_DELAY_UI);
         sensorManager.registerListener(listener, accSensor, SensorManager.SENSOR_DELAY_UI);
     }
-    private Object radian2Degree(float radian) {
-        return  radian * 180 /(float)Math.PI;
+    private float radian2Degree(float radian) {
+        return radian * 100 / (float) Math.PI;
     }
 }
